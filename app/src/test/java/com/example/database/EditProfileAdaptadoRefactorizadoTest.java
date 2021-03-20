c9707cb696e693b975762e8fb7bb8f9a17bf773c
Andy <andy.nuzzi@gmail.com>
2021-03-20 13:37:48 +0100

    Arreglar el rename de las clases del test

--- app/src/test/java/com/example/database/EditProfileAdaptadoRefactorizadoTest.java
+++ app/src/test/java/com/example/database/EditProfileAdaptadoRefactorizadoTest.java
@@ -6,21 +6,21 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
-public class EditProfileTest {
+public class EditProfileAdaptadoRefactorizadoTest {
 
     @Test
     public void saveChanges2() {
         ArrayList<String> alResultado = new ArrayList<>();
-        alResultado = EditProfile.saveChanges2("", "123", "123",
+        alResultado = EditProfileAdaptadoRefactorizado.saveChanges2("", "123", "123",
                 "a", "b", "b@gmail.com");
         assertEquals( "DebesIniciarSesionPreviamente",alResultado.get(0));
 
 
-        alResultado = EditProfile.saveChanges2("a@gmail.com", "123", "456",
+        alResultado = EditProfileAdaptadoRefactorizado.saveChanges2("a@gmail.com", "123", "456",
                 "a", "b", "b@gmail.com");
         assertEquals( "ContraseñaNoCoincide(1)",alResultado.get(0));
 
-        alResultado = EditProfile.saveChanges2("a@gmail.com", "123", "123",
+        alResultado = EditProfileAdaptadoRefactorizado.saveChanges2("a@gmail.com", "123", "123",
                 "", "b", "b@gmail.com");
         assertEquals( "123",alResultado.get(0));
         assertEquals("b",alResultado.get(1));
@@ -29,13 +29,13 @@ public class EditProfileTest {
         assertEquals("goMain(1)",alResultado.get(4));
         ////todos los tesst realizados "oficiales", ahora tocan todos los q faltan
 
-        alResultado = EditProfile.saveChanges2("a@gmail.com", "123", "123",
+        alResultado = EditProfileAdaptadoRefactorizado.saveChanges2("a@gmail.com", "123", "123",
                 "", "", "");
         assertEquals("123",alResultado.get(0));
         assertEquals("CambiosReliados(1)",alResultado.get(1));
         assertEquals("goMain(1)",alResultado.get(2));
 
-        alResultado = EditProfile.saveChanges2("a@gmail.com", "123", "123",
+        alResultado = EditProfileAdaptadoRefactorizado.saveChanges2("a@gmail.com", "123", "123",
                 "a", "", "b@gmail.com");
         assertEquals( "123",alResultado.get(0));
         assertEquals("a",alResultado.get(1));
--- app/src/test/java/com/example/database/EditProfileOriginalAdaptadoTest.java
+++ app/src/test/java/com/example/database/EditProfileOriginalAdaptadoTest.java
@@ -6,20 +6,20 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
-public class EditProfile2Test {
+public class EditProfileOriginalAdaptadoTest {
 
     @Test
     public void saveChanges() {
         ArrayList<String> alResultado = new ArrayList<>();
-        alResultado = EditProfile2.saveChanges("", "123", "123",
+        alResultado = EditProfileOriginalAdaptado.saveChanges("", "123", "123",
                 "a", "b", "b@gmail.com");
         assertEquals( "DebesIniciarSesionPreviamente",alResultado.get(0));
 
-        alResultado = EditProfile2.saveChanges("a@gmail.com", "123", "456",
+        alResultado = EditProfileOriginalAdaptado.saveChanges("a@gmail.com", "123", "456",
                 "a", "b", "b@gmail.com");
         assertEquals( "ContraseñaNoCoincide(1)",alResultado.get(0));
 
-        alResultado = EditProfile2.saveChanges("a@gmail.com", "123", "123",
+        alResultado = EditProfileOriginalAdaptado.saveChanges("a@gmail.com", "123", "123",
                 "", "b", "b@gmail.com");
         assertEquals( "123",alResultado.get(0));
         assertEquals("b",alResultado.get(1));
@@ -28,13 +28,13 @@ public class EditProfile2Test {
         assertEquals("goMain(1)",alResultado.get(4));
         ////todos los tesst realizados "oficiales", ahora tocan todos los q faltan
 
-        alResultado = EditProfile2.saveChanges("a@gmail.com", "123", "123",
+        alResultado = EditProfileOriginalAdaptado.saveChanges("a@gmail.com", "123", "123",
                 "", "", "");
         assertEquals("123",alResultado.get(0));
         assertEquals("CambiosReliados(1)",alResultado.get(1));
         assertEquals("goMain(1)",alResultado.get(2));
 
-        alResultado = EditProfile2.saveChanges("a@gmail.com", "123", "123",
+        alResultado = EditProfileOriginalAdaptado.saveChanges("a@gmail.com", "123", "123",
                 "a", "", "b@gmail.com");
         assertEquals( "123",alResultado.get(0));
         assertEquals("a",alResultado.get(1));
